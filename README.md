cb978758f6a0858c6c1373e8963754f45451e61a
baba_s <shotababa1206@gmail.com>
2020-08-17 09:56:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UniIDisposableExtensionMethods
---
+++ Scripts/IDisposableExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kogane
+{
+	/// <summary>
+	/// IDisposable 型の拡張メソッドを管理するクラス
+	/// </summary>
+	public static class IDisposableExt
+	{
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// 指定された IDisposable 型のインスタンスが null ではない場合にのみ Dispose を実行します
+		/// </summary>
+		public static void DisposeIfNotNull( this IDisposable self )
+		{
+			self?.Dispose();
+		}
+	}
+}
